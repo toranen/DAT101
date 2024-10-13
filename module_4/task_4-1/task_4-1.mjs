fd16c6e7fd65da35f863ffbeda5c1bcc3acfe38e
toranen <tor.anen05@gmail.com>
2024-10-13 15:29:41 +0200

    4-1 Part 1

--- module_4/task_4-1/task_4-1.mjs
+++ module_4/task_4-1/task_4-1.mjs
@@ -4,35 +4,57 @@ initPrintOut(document.getElementById("txtOut"));
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+console.log("v Part 1 v");
+
+const accountType = {
+    normal: "Brukskonto",
+    saving: "Sparekonto",
+    credit: "Kreditkonto",
+    pension: "pensionskonto",
+}
+printOut(accountType.normal + ", " + accountType.saving + ", " + accountType.credit + ", " + accountType.pension);
+
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+console.log("v Part 2 v");
+
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+console.log("v Part 3 v");
+
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+console.log("v Part 4 v");
+
 printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+console.log("v Part 5 v");
+
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+console.log("v Part 6 v");
+
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+console.log("v Part 7 v");
+
 printOut(newLine);
