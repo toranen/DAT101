11a50394ebbd4c5ba3f1060b58e2176db2910ae8
toranen <tor.anen05@gmail.com>
2024-12-15 13:35:35 +0100

    4-2 completed

--- module_4/task_4-2/task_4-2.mjs
+++ module_4/task_4-2/task_4-2.mjs
@@ -6,6 +6,8 @@ printOut("--- Part 1 -----------------------------------------------------------
 /* Put your code below here!*/
 console.log(`v Part 1 v`)
 
+let date = Date.now();
+
 const numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
 for (let i = 0; numberList[i] <= numberList[19]; i++) {
@@ -46,7 +48,7 @@ let names = ["Anne", "Inger", "Kari", "Marit", "Ingrid", "Liv", "Eva", "Berit", 
 "Bjørg", "Hilde", "Anna", "Solveig", "Marianne", "Randi", "Ida", "Nina", "Maria", "Elisabeth", "Kristin"];
 
 function removeElement(names, index){
- 
+
     console.log(index, "index")
     let word = names[index];
     console.log(word, "element word")
@@ -111,8 +113,7 @@ class TBook {
                 let book = this.#title;
                 let author = this.#author
                 let isbn = this.#isbn
-                this.toString(this.#title, this.#author, this.#isbn)
-                break
+                return this.toString(this.#title, this.#author, this.#isbn);
             }
             case "Shogun": {
                 this.#title = "Shogun";
@@ -121,8 +122,7 @@ class TBook {
                 let book2 = this.#title;
                 let author2 = this.#author
                 let isbn2 = this.#isbn
-                this.toString(this.#title, this.#author, this.#isbn)
-                break
+                return this.toString(this.#title, this.#author, this.#isbn);
             }
             case "Looking Forward": {
                 this.#title = "Looking Forward";
@@ -131,8 +131,7 @@ class TBook {
                 let book3 = this.#title;
                 let author3 = this.#author;
                 let isbn3 = this.#isbn;
-                this.toString(this.#title, this.#author, this.#isbn)
-                break
+                return this.toString(this.#title, this.#author, this.#isbn);
             }
             default: {
                 console.log(`This book is not in the bookshelf. Please try another book.`);
@@ -142,51 +141,159 @@ class TBook {
     }
 
     toString() {
-        if (this.#title === "Harry Potter"){
-            printOut(`${this.#title}, Author: ${this.#author}, ISBN: ${this.#isbn}`);
-            return (`Harry Potter, Author: J. K. Rowling, ISBN: harrypotterISBN`);
-        } else if (this.#title === "Shogun"){
-            printOut(`${this.#title}, Author: ${this.#author}, ISBN: ${this.#isbn}`);
-            return (`Shogun, Author: James Clavell, ISBN: shogunISBN`);
-        } else if (this.#title === "Looking Forward"){
-            printOut(`${this.#title}, Author: ${this.#author}, ISBN: ${this.#isbn}`);
-            return (`Looking Forward, Author: Marcia Willett, ISBN: lookingforwardISBN`);
+
+        switch (this.#title){
+            case "Harry Potter": {
+                printOut(`${this.#title}, Author: ${this.#author}, ISBN: ${this.#isbn}`);
+                return (`${this.#title}, Author: ${this.#author}, ISBN: ${this.#isbn}`);
+            }
+            case "Shogun": {
+                printOut(`${this.#title}, Author: ${this.#author}, ISBN: ${this.#isbn}`);
+                return (`${this.#title}, Author: ${this.#author}, ISBN: ${this.#isbn}`);
+            }
+            case "Looking Forward": {
+                printOut(`${this.#title}, Author: ${this.#author}, ISBN: ${this.#isbn}`);
+                return (`${this.#title}, Author: ${this.#author}, ISBN: ${this.#isbn}`);
+            }
+            default: {
+                return (`This book is not in the bookshelf. Please try another book.`);
+            }
         }
     }
 }
 
 const classTBook = ["Harry Potter", "Shogun", "Looking Forward"];
-const bookshelf = new TBook("Harry Potter")
+const bookshelf = new TBook();
 for (let i = 0; i < classTBook.length; i++){
+    console.log(i);
     console.log(bookshelf.setBook(classTBook[i]));
-    console.log(`${classTBook[i]}`);
-    console.log(i)
 }
-
 
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
 console.log(`v Part 7 v`)
-printOut("Replace this with you answer!");
+
+const EWeekDays = {
+    WeekDay1: {value: 0x01, name: "Monday"},
+    WeekDay2: {value: 0x02, name: "Tuesday"},
+    WeekDay3: {value: 0x04, name: "Wednesday"},
+    WeekDay4: {value: 0x08, name: "Thursday"},
+    WeekDay5: {value: 0x10, name: "Friday"},
+    WeekDay6: {value: 0x20, name: "Saturday"},
+    WeekDay7: {value: 0x40, name: "Sunday"},
+    Workdays: {value: 0x01 + 0x02 + 0x04 + 0x08 + 0x10, name: "Workdays"},
+    Weekends: {value: 0x20 + 0x40, name: "Weekend"},
+};
+
+const keys = Object.keys(EWeekDays);
+
+keys.forEach(key => {
+    printOut(`${key}: value: ${EWeekDays[key].value}, name: ${EWeekDays[key].name}`);
+    console.log(`${key}:`, EWeekDays[key]); 
+});
+
 printOut(newLine);
 
-printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
+printOut("--- Part 8, 9 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-console.log(`v Part 8 v`)
-printOut("Replace this with you answer!");
-printOut(newLine);
+console.log(`v Part 8, 9 v`)
 
-printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-console.log(`v Part 9 v`)
-printOut("Replace this with you answer!");
+const randomNumbers = Array.from({ length: 35 }, () => Math.floor(Math.random() * 20) + 1);
+printOut(`Original Array: ${randomNumbers}`);
+console.log(`Original Array: ${randomNumbers}`);
+
+const ascendingOrder = [...randomNumbers].sort((a, b) => a - b);
+console.log("Ascending Order:", ascendingOrder);
+printOut(`Ascending order: ${ascendingOrder}`);
+
+const descendingOrder = [...randomNumbers].sort((a, b) => b - a);
+console.log("Descending Order:", descendingOrder);
+printOut(`Descending order: ${descendingOrder}`);
+
+const frequencyMap = {};
+randomNumbers.forEach((num) => {
+  frequencyMap[num] = (frequencyMap[num] || 0) + 1;
+});
+
+const frequencyArray = Object.entries(frequencyMap).map(([num, freq]) => ({
+  number: parseInt(num),
+  frequency: freq,
+}));
+
+frequencyArray.sort((a, b) => {
+  if (b.frequency === a.frequency) {
+    return a.number - b.number;
+  }
+  return b.frequency - a.frequency;
+});
+printOut(newLine);
+console.log("\nNumbers and Their Frequency:");
+printOut(`Numbers and Their Frequency:`);
+frequencyArray.forEach(({ number, frequency }) => {
+  console.log(`Number: ${number}, Frequency: ${frequency}`);
+  printOut(`Number: ${number}, Frequency: ${frequency}`);
+});
+
+const groupedByFrequency = {};
+frequencyArray.forEach(({ number, frequency }) => {
+  if (!groupedByFrequency[frequency]) {
+    groupedByFrequency[frequency] = [];
+  }
+  groupedByFrequency[frequency].push(number);
+});
+
+const sortedFrequencies = Object.entries(groupedByFrequency)
+  .map(([freq, numbers]) => ({
+    frequency: parseInt(freq),
+    numbers: numbers.sort((a, b) => a - b),
+  }))
+  .sort((a, b) => b.frequency - a.frequency);
+
+printOut(newLine);
+console.log("\nFrequencies and Corresponding Numbers:");
+printOut(`Frequencies and Corresponding Numbers:`);
+sortedFrequencies.forEach(({ frequency, numbers }) => {
+  console.log(`Frequency: ${frequency}, Numbers: ${numbers.join(", ")}`);
+  printOut(`Frequency: ${frequency}, Numbers: ${numbers.join(", ")}`);
+});
+
 printOut(newLine);
 
 /* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
 console.log(`v Part 10 v`)
-printOut("Replace this with you answer!");
+
+const array1 = [];
+
+for (let row = 0; row < 5; row++) {
+
+  const rowArray1 = [];
+
+  for (let col = 0; col < 9; col++) {
+    rowArray1.push(`Row ${row + 1}, Col ${col + 1}`);
+  }
+
+  array1.push(rowArray1);
+}
+
+console.log("Array with rows and columns:");
+printOut(`Array with rows and columns:`);
+
+for (let row = 0; row < array1.length; row++) {
+  let rowText = "";
+  for (let col = 0; col < array1[row].length; col++) {
+    rowText += array1[row][col] + "\t";
+  }
+  console.log(rowText);
+  printOut(`${rowText}`);
+}
+
+let endDate = Date.now();
+
+let runtime = endDate - date;
+console.log(`${runtime} ms`);
+
 printOut(newLine);
